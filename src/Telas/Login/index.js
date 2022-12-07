2f2ef198e8d1b71d34518b06bfba852b0ad5b453
Ibanez Fernandes <ibanezjunior@alunos.utfpr.edu.br>
2022-12-06 23:15:17 -0300

    Ajuste da chamada de banco

--- src/Telas/Login/index.js
+++ src/Telas/Login/index.js
@@ -7,51 +7,64 @@ export default Login = (props) => {
 
     const [usuario, setUsuario] = useState("");
     const [senha, setSenha] = useState("");
+    const [status, setStatus] = useState(false);
 
-    useEffect(() => {
-        async function carregaDados() {
-            await firebase.database().ref().on('value', (snapshot) => {
-                setUsuario(snapshot.val().usuario);
-                setSenha(snapshot.val().senha);
+    async function validarDados() {
+        if (usuario !== "") {
+            await firebase.database().ref('Usuario').child(usuario).once('value', (snapshot) => {
+                if (snapshot.val() !== null) {
+                    if (snapshot.val().senha === senha) {
+                        setStatus(true);
+                    } else {
+                        alert("Senha Inválida");    
+                    }
+                } else {
+                    alert("Usuário não existe");
+                }
             });
         }
-        carregaDados();
-    }, []);
-    
+    }
+
     return(
         <View style={Estilos.caixa}>
-            <Text style={Estilos.textoG}>Login</Text>
-            <TextInput style={Estilos.entrada}
-                placeholder="Digite seu Usuário"
-                onChangeText={setUsuario}
-                value={usuario}
-            />
-            <TextInput style={Estilos.entrada}
-                placeholder="Digite sua Senha"
-                secureTextEntry={true}
-                onChangeText={setSenha}
-                value={senha}
-            />
-            <Pressable style={Estilos.pressionar}>
-                <Text style={Estilos.textoGB}>
-                <Image
-                    source={require("../../../assets/botao_urso.png")}
-                    style={Estilos.botao}
-                /> Entrar</Text>
-            </Pressable>
-            <View style={Estilos.pressionar}>
-                <Pressable>
-                    <Text style={Estilos.textoMB}>Cadastrar</Text>
-                </Pressable>
-                <Text style={Estilos.textoMB2}></Text>
-                <Pressable>
-                    <Text style={Estilos.textoMB}>Reset da Senha</Text>
-                </Pressable>
-            </View>
-            <View>
-                <Text>{usuario}</Text>
-                <Text>{senha}</Text>
-            </View>
+            {
+                status ? (
+                    <>
+                        <Text>Usuário: {usuario}, logado com sucesso!!!</Text>
+                    </>
+                ) : (
+                    <>
+                        <Text style={Estilos.textoG}>Login</Text>
+                        <TextInput style={Estilos.entrada}
+                            placeholder="Digite seu Usuário"
+                            onChangeText={setUsuario}
+                            value={usuario}
+                        />
+                        <TextInput style={Estilos.entrada}
+                            placeholder="Digite sua Senha"
+                            secureTextEntry={true}
+                            onChangeText={setSenha}
+                            value={senha}
+                        />
+                        <Pressable style={Estilos.pressionar} onPress={validarDados}>
+                            <Text style={Estilos.textoGB}>
+                            <Image
+                                source={require("../../../assets/botao_urso.png")}
+                                style={Estilos.botao}
+                            /> Entrar</Text>
+                        </Pressable>
+                        <View style={Estilos.pressionar}>
+                            <Pressable>
+                                <Text style={Estilos.textoMB}>Cadastrar</Text>
+                            </Pressable>
+                            <Text style={Estilos.textoMB2}></Text>
+                            <Pressable>
+                                <Text style={Estilos.textoMB}>Reset da Senha</Text>
+                            </Pressable>
+                        </View>
+                    </>
+                )
+            }
         </View>
     );
 }
--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,14 +2,14 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/database';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCT68lOSl8Sf5urKjzG6DCM3B4eofbOYqk",
-    authDomain: "novolocalab.firebaseapp.com",
-    databaseURL: "https://novolocalab-default-rtdb.firebaseio.com/",
-    projectId: "novolocalab",
-    storageBucket: "novolocalab.appspot.com",
-    messagingSenderId: "720341766167",
-    appId: "1:720341766167:web:bb2814347efb1dd2350c52",
-}
+    apiKey: "AIzaSyCFhpOZHDX_xoMYqyuXA28kH-OeaQmpYf4",
+    authDomain: "localab2.firebaseapp.com",
+    projectId: "localab2",
+    databaseURL: "https://localab2-default-rtdb.firebaseio.com/",
+    storageBucket: "localab2.appspot.com",
+    messagingSenderId: "873144839147",
+    appId: "1:873144839147:web:fc5eb5e5182bd34e2a87ae"
+};
 
 if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
